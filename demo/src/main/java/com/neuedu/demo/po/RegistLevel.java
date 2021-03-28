9f1a60915027be066b4ae0e0332de1aee620066e
clonetx <937805333@qq.com>
2021-03-28 14:40:02 +0800

    二次上传：挂号级别实体类

---
+++ demo/src/main/java/com/neuedu/demo/po/RegistLevel.java
@@ -0,0 +1,59 @@
+package com.neuedu.demo.po;
+
+public class RegistLevel {
+	private int id;
+	private String registCode;
+	private String registName;
+	private int sequenceNo;
+	private double registFee;
+	private int registQuota;
+	private int delMark;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getRegistCode() {
+		return registCode;
+	}
+	public void setRegistCode(String registCode) {
+		this.registCode = registCode;
+	}
+	public String getRegistName() {
+		return registName;
+	}
+	public void setRegistName(String registName) {
+		this.registName = registName;
+	}
+	public int getSequenceNo() {
+		return sequenceNo;
+	}
+	public void setSequenceNo(int sequenceNo) {
+		this.sequenceNo = sequenceNo;
+	}
+	public double getRegistFee() {
+		return registFee;
+	}
+	public void setRegistFee(double registFee) {
+		this.registFee = registFee;
+	}
+	public int getRegistQuota() {
+		return registQuota;
+	}
+	public void setRegistQuota(int registQuota) {
+		this.registQuota = registQuota;
+	}
+	public int getDelMark() {
+		return delMark;
+	}
+	public void setDelMark(int delMark) {
+		this.delMark = delMark;
+	}
+	
+	
+	
+	
+	
+
+}
